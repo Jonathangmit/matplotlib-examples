768ef6a7efd9505adf8a49fb5d7932b7ad533999
unknown <g00387830@gmit.ie>
2020-10-10 17:38:46 +0100

    plot examples

---
+++ 2dplot2.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.arange(0.0, 10.0,0.01)
+y = 3.0* x + 1.0
+
+noise = np.random.normal(0.0, 1.0, len(x))
+plt.plot(x,y + noise, 'r.')
+#allows a second plot to be made on the same plot
+plt.plot(x,y,'b-')
+plt.show()
+#clears plot
+plt.plot(x,y + noise, 'g.')
+plt.plot(x,y,'c-')
+plt.show()
